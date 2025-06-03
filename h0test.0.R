145d462a7109f8ff47cedbc74ec8ff57b6a77035
Mitch Kostich <mitch.kostich@jax.org>
2025-06-03 09:57:49 -0400

    added f.impute_glm_binom() and f.impute_loess_logit()

--- h0test.0.R
+++ h0test.0.R
@@ -31,70 +31,77 @@
 ## f.filter_samples(exprs, samps, feats, n_features_min=1000)
 ##   return(list(exprs=exprs, samps=samps, feats=feats))
 ## 
-## f.samples_per_feature(exprs)
-##   return(n)
+## f.samples_per_feature(exprs) return(n)
 ##
-## f.feature_median_expression(exprs)
-##   return(m)
+## f.feature_median_expression(exprs) return(m)
 ##
-## f.features_per_sample(exprs)
-##   return(n)
+## f.features_per_sample(exprs) return(n)
 ##
 ## Inter-sample edger (tmm) normalization;
 ##   normalized cpm values using edger norm factors
-##   method in c("TMM", "TMMwsp", "RLE", "upperquartile", "none")
-## f.normalize_edger(exprs, method="TMM") 
-##   return(exprs)
-## 
+##   f.normalize_edger(exprs, method="TMM") return(exprs)
+##     method in c("TMM", "TMMwsp", "RLE", "upperquartile", "none")
 ## Inter-sample quantile normalization; same as limma scale normalization 
 ##   when norm_quantile == 0.5 (median); multiplier like with cpm:
-## f.normalize_quantile <- function(exprs, norm_quantile=0.5, multiplier=1e6)
-##   return(exprs)
+##   f.normalize_quantile <- function(exprs, norm_quantile=0.5, multiplier=1e6) return(exprs)
 ## 
 ## Counts per million (if multiplier is 1e6):
-## f.normalize_cpm(exprs, multiplier=1e6)
-##   return(exprs)
+##   f.normalize_cpm(exprs, multiplier=1e6) return(exprs)
 ## 
-## Variance stabilizing normalization:
-##   results on log2 scale:
-## f.normalize_vsn(exprs)
-##   return(exprs)
+## Variance stabilizing normalization: results on log2 scale:
+##   f.normalize_vsn(exprs) return(exprs)
 ## 
 ## Cyclic loess normalization; 
-##   method in c("fast", "affy", "pairs"); 
-##   slow; fast is linear, others are quadratic:
-## f.normalize_loess <- function(exprs, span=0.7, method="affy")
-##   return(exprs)
-## 
+##   f.normalize_loess <- function(exprs, span=0.7, method="affy") return(exprs)
+##     method in c("fast", "affy", "pairs"); 
+##     slow; fast is linear, others are quadratic:
+##
 ## Impute missing values assuming left-censored only.
-##   impute_quantile: quantile of minimum non-zero expression value for each 
-##     gene to use as global LOD. 
 ##   Returns expression matrix same as exprs but w/ missing values imputed.
 ##     ONLY NA VALUES CONSIDERED MISSING, so may need to first:
 ##       exprs[exprs == 0] <- NA
 ##   Imputed values are random draws from uniform in [0, LOD].
-## f.impute_unif_global_lod(exprs, impute_quantile=0.01)
-##   return(exprs)
+##   f.impute_unif_global_lod(exprs, impute_quantile=0.01) return(exprs)
+##     impute_quantile: quantile of minimum non-zero expression value for each 
+##       gene to use as global LOD. 
 ## 
 ## for each sample, convert NAs to minimum non-NA value in that sample:
-## f.impute_sample_lod(exprs)
-##   return(exprs)
+##   f.impute_sample_lod(exprs) return(exprs)
 ## 
 ## for each sample, impute random draws from:
 ##   uniform in (0, quantile(exprs[, sample], probs=impute_quantile))
-## f.impute_unif_sample_lod(exprs, impute_quantile=0.01)
-##   return(exprs)
-## 
+##   f.impute_unif_sample_lod(exprs, impute_quantile=0.01) return(exprs)
+##
 ## impute random draws from:
 ##   normal(mean=mean(exprs[feature, ]), sd=scale.*sd(exprs[feature, ])):
-## f.impute_rnorm_feature(exprs, scale.=1)
-##   return(exprs)
+##   f.impute_rnorm_feature(exprs, scale.=1) return(exprs)
+##
+## impute random draws by estimating p(missing|intensity) using a 
+##   binomial glm of form cbind(n.missing, n.found) ~ intensity,
+##     where intensity is assumed to be previously log transformed.
+##   f.impute_glm_binom(x, gran=0.0001, off=1, f_mid=median) return(x)
+##     x: numeric matrix with feature rows and observation columns;
+##     gran: granularity of predictions (smaller = less chance of dups); numeric > 0
+##     off: offset for p.missing = (n.miss + off)/(n.total + off); numeric;
+##     f_mid: function for aggregating intensities into a central estimate; function
+##
+## impute random draws by estimating p(missing|intensity) using a 
+##   loess fit of form log(p_missing/p_found) ~ intensity,
+##     where intensity is assumed to be previously log transformed.
+##   f.impute_loess_logit(x, gran=0.0001, off=1, f_mid=median, 
+##       span=0.25, degree=1, fam="symmetric") return(x)
+##     x: numeric matrix with feature rows and observation columns;
+##     gran: granularity of predictions (smaller = less chance of dups); numeric > 0
+##     off: offset for p.missing = (n.miss + off)/(n.total + off); numeric;
+##     f_mid: function for aggregating intensities into a central estimate; function
+##     span: span for loess fit; numeric in closed interval (0, 1)
+##     degree: degree for loess fit; integer in c(1, 2)
+##     fam: family for loess fit; character in c("gaussian", "symmetric")
 ## 
-## f.test_voom <- function(exprs, samps, frm, test_var, normalize.method="none")
+## f.test_voom <- function(exprs, samps, frm, test_var, normalize.method="none") 
 ##   return(tbl)
 ## 
-## f.test_trend <- function(exprs, samps, frm, test_var)
-##   return(tbl)
+## f.test_trend <- function(exprs, samps, frm, test_var) return(tbl)
 
 
 ###############################################################################
@@ -378,9 +385,71 @@ f.impute_rnorm_feature <- function(exprs, scale.=1) {
   return(exprs)
 }
 
-## impute random draws by ...:
+## impute random draws by estimating p(missing|intensity) using a 
+##   binomial glm of form cbind(n.missing, n.found) ~ intensity,
+##     where intensity is assumed to be previously log transformed.
+##   f.impute_glm_binom(x, gran=0.0001, off=1, f_mid=median) return(x)
+##     x: numeric matrix with feature rows and observation columns;
+##     gran: granularity of predictions (smaller = less chance of dups); numeric > 0
+##     off: offset for p.missing = (n.miss + off)/(n.total + off); numeric;
+##     f_mid: function for aggregating intensities into a central estimate; function
 
+f.impute_glm_binom <- function(x, gran=0.0001, off=1, f_mid=median) {
 
+  if(!is.matrix(x)) stop("!is.matrix(x)")
+  if(!is.numeric(x)) stop("!is.numeric(x)")
+
+  m <- apply(x, 1, f_mid, na.rm=T)
+  m[is.na(m)] <- 0
+  n0 <- apply(x, 1, function(v) sum(is.na(v) | v %in% 0))  ## n.missing
+  n1 <- ncol(x) - n0                                     
+  p <- (n0 + off) / (ncol(x) + off)
+  dat <- data.frame(n0=n0, n1=n1, p=p, m=m)
+
+  fit <- glm(cbind(n0, n1) ~ m, data=dat, family="binomial")
+  m <- seq(from=gran, to=max(c(x), na.rm=T), by=gran)
+  p <- predict(fit, newdata=data.frame(m=m), type="response")
+  p[is.na(p)] <- min(p, na.rm=T)
+  x[is.na(c(x))] <- sample(m, sum(is.na(c(x))), replace=T, prob=p)
+
+  return(x)
+}
+
+## impute random draws by estimating p(missing|intensity) using a 
+##   loess fit of form log(p_missing/p_found) ~ intensity,
+##     where intensity is assumed to be previously log transformed.
+##   f.impute_loess_logit(x, gran=0.0001, off=1, f_mid=median, 
+##       span=0.25, degree=1, fam="symmetric") return(x)
+##     x: numeric matrix with feature rows and observation columns;
+##     gran: granularity of predictions (smaller: less chance of duplicates); numeric > 0
+##     off: offset for probability.missing = (n.miss + off)/(n.total + off); numeric;
+##     f_mid: function for aggregating intensities into a central estimate; function
+##     span: span for loess fit; numeric in closed interval (0, 1)
+##     degree: degree for loess fit; integer in c(1, 2)
+##     fam: family for loess fit; character in c("gaussian", "symmetric")
+
+f.impute_loess_logit <- function(x, gran=0.0001, off=1, f_mid=median, 
+    span=0.25, degree=1, fam="symmetric") {
+
+  if(!is.matrix(x)) stop("!is.matrix(x)")
+  if(!is.numeric(x)) stop("!is.numeric(x)")
+
+  m <- apply(x, 1, f_mid, na.rm=T)
+  m[is.na(m)] <- 0
+  n0 <- apply(x, 1, function(v) sum(is.na(v) | v %in% 0))  ## n.missing
+  n1 <- ncol(x) - n0                                       ## n.found
+  p <- (n0 + off) / (ncol(x) + off)                        ## p.missing
+  dat <- data.frame(n0=n0, n1=n1, p=p, m=m)
+
+  fit <- loess(log(p/(1-p)) ~ m, data=dat, span=span, degree=degree, family=fam)
+  m <- seq(from=gran, to=max(c(x), na.rm=T), by=gran)      ## newdata
+  p <- predict(fit, newdata=data.frame(m=m))               ## on logit scale
+  p[is.na(p)] <- min(p, na.rm=T)
+  p = exp(p) / (1 + exp(p))                                ## inverse logit
+  x[is.na(c(x))] <- sample(m, sum(is.na(c(x))), replace=T, prob=p)
+
+  return(x)
+}
 
 
 ###############################################################################
